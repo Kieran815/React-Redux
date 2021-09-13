0230ba2300fb581c2055d161e3dd7a10383b0592
Kieran <kieran.milligan@gmail.com>
2021-09-13 09:34:20 -0500

    finished dropdown widget

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -33,17 +33,29 @@ const options = [
   }
 ]
 
-export default () => {
+const App = () => {
 
   const [selected, setSelected ] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
 
   return(
     <div>
       <br/>
+      <button onClick={() => {setShowDropdown(!showDropdown)}}>
+        Toggle Dropdown
+      </button>
+
       {/* <Accordion items={items} /> */}
       {/* <Search /> */}
-      <Dropdown options={options} selected={selected} onSelectedChange={setSelected} />
+      {showDropdown ?
+      <Dropdown
+        options={options}
+        selected={selected}
+        onSelectedChange={setSelected}
+      /> : null}
     </div>
     
   )
 }
+
+export default App;
--- hooks/src/components/Dropdown.js
+++ hooks/src/components/Dropdown.js
@@ -1,8 +1,36 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useRef } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
 
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
+
+  // `useRef` creates a reference point inside a DOM element
+  const ref = useRef();
+
+  useEffect(() => {
+    const onBodyClick = event => {
+      // if clicked element is INSIDE the component...
+      if (ref.current.contains(event.target)) {
+        // exit callback and return early
+        return;
+      }
+      setOpen(false);
+    };
+    // add listener to run `onBodyClick`
+    document.body.addEventListener(
+      "click",
+      onBodyClick,
+      { capture: true }
+    );
+    // cleanup to remove `onBodyClick` to prevent `null.contains` error
+    return () => {
+      // remove listener from DOM element
+      document.body.removeEventListener(
+        'click',
+        onBodyClick,
+        { capture: true});
+    };
+  }, []);
 
   const renderedOptions = options.map((option) => {
     if (option.value === selected.value) {
@@ -22,14 +50,24 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     );
   })
 
+  
+  console.log(ref.current);
+
   return (
-    <div className="ui form">
+    <div
+      ref={ref}
+      className="ui form"
+    >
       <div className="field">
         <label className="label">
           Select a Color
         </label>
         <div
-          onClick={() => setOpen(!open)}
+          // toggle open
+          onClick={() => {
+            setOpen(!open);
+          }}
+          // ternary statement to open/close dropdown
           className={`ui selection dropdown ${open ? 'visible active' : ''}`}
         >
           <i className="dropdown icon"></i>
